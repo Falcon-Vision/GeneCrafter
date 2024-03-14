1471aa42ef644f05a26fc141d4b53d75f1125935
Veerendra Hari Masina <veerendrahari@gmail.com>
2024-03-14 07:12:05 -0400

    added skeleton projs

---
+++ src/Api/GeneCrafter.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeneCrafter.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger) => _logger = logger;
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            //bool temp = "tets".IsTrue();
+
+            WeatherForecast[] yest =
+
+             Enumerable.Range(1, 5).Select(index => new WeatherForecast
+             {
+                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                 TemperatureC = Random.Shared.Next(-20, 55),
+                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+             })
+            .ToArray();
+
+            return yest;
+        }
+    }
+}
---
+++ src/Common/GeneCrafter.Utility/Test.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Standardization.Libraries.Extensions;
+
+namespace GeneCrafter.Utility
+{
+    public static class Test
+    {
+        public static bool IsValid() => "hello".IsTrue();
+    }
+}
